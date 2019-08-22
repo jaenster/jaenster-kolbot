66dfc6e017aa5a37973ce4be4f342dce5662ace9
jaenster <github@jaenster.net>
2019-08-22 14:26:58 +0200

    updated the fields of hashing a bit

--- d2bs/kolbot/libs/unit/Item.js
+++ d2bs/kolbot/libs/unit/Item.js
@@ -1,5 +1,5 @@
 (function () {
-	const fields = ['x', 'y', 'sizex', 'sizey', 'classid', 'hpmax', 'itemcount', 'ownertype', 'uniqueid', 'code', 'prefixnum', 'suffixnum', 'prefixnums', 'suffixnums', 'fname,quality', 'node', 'location', 'sizex', 'sizey', 'description', 'ilvl', 'lvlreq', 'gfx'];
+	const fields = ['x', 'y', 'sizex', 'sizey', 'classid', 'hpmax', 'itemcount', 'uniqueid', 'code', 'fname', 'quality', 'node', 'location', 'description', 'ilvl', 'lvlreq', 'gfx'];
 	Object.defineProperty(Unit.prototype, 'uniqueId', {
 		get: function () {
 			let text = fields.map(key => (this.hasOwnProperty(key) && this[key] || '').toString()).filter(x => x);
