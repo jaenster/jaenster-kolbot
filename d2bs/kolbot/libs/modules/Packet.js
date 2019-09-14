18ff953c4fcd4848d945719bc64452d63624836a
jaenster <github@jaenster.net>
2019-09-14 12:26:53 +0200

    Carcass for Packet module

---
+++ d2bs/kolbot/libs/modules/Packet.js
@@ -0,0 +1,21 @@
+/**
+ * @description Packet handlers. Event driven packets.
+ * @author Jaenster
+ */
+
+
+{ // blocked scope
+	switch (getScript.startAsThread()) {
+		case 'thread':
+			(function (require) {
+				const Packet = require('require.js');
+			})(include('require.js') && require);
+			break;
+		case 'loaded':
+		case 'started':
+			print('thread started/loaded');
+			(function (module, require) {
+
+			})(module, require);
+	}
+}
