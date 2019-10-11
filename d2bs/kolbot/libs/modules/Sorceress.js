8b2b1c259e6b6bcc82b3187259b3ddc408a02bfe
jaenster <github@jaenster.net>
2019-10-11 13:38:58 +0200

    Dont use telekenis if you possess the skill

--- d2bs/kolbot/libs/modules/Sorceress.js
+++ d2bs/kolbot/libs/modules/Sorceress.js
@@ -8,7 +8,7 @@
 	(function (interact) {
 		Unit.prototype.interact = function (...args) {
 			// Dont use on anything else as objects, or if any argument given
-			if (this.type !== sdk.unittype.Objects || args.length) interact.apply(this, args);
+			if (this.type !== sdk.unittype.Objects || args.length || !me.getSkill(sdk.skills.Telekinesis, 1)) return interact.apply(this, args);
 
 			this.cast(sdk.skills.Telekinesis, undefined, undefined, undefined, undefined, true);
 		}
@@ -19,6 +19,7 @@
 		Misc.click = function (...args) {
 			let [button, shift, unit] = args;
 			switch (true) { // A big list of exclusions
+				case !me.getSkill(sdk.skills.Telekinesis, 1):
 				case typeof unit !== 'object':
 				case unit.type !== sdk.unittype.Objects:
 				case unit.name === 'portal' && !me.inTown: // cant use telekenis on portals
