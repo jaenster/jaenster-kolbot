876c363db9ff8b62aef2e0e6f2582c861d7b3478
jaenster <github@jaenster.net>
2019-10-28 17:26:39 +0100

    Added socket module

---
+++ d2bs/kolbot/libs/modules/Socket.js
@@ -0,0 +1,66 @@
+/**
+ * @description a wrapper around the socket object
+ * @author Jaenster
+ */
+
+
+(function (module, require, buildinSock) {
+	const Worker = require('Worker');
+	const Events = require('Events');
+
+	function Socket(hostname, port) {
+		typeof Socket.__socketCounter === 'undefined' && (Socket.__socketCounter = 0);
+
+		const myEvents = new Events;
+
+		this.buffer = '';
+		this.connect = () => (this.socket = buildinSock.open(hostname, port) && false) || this;
+
+		this.on = myEvents.on;
+		this.off = myEvents.off;
+		this.once = myEvents.once;
+
+		const close = () => {
+			this.socket = null;
+			myEvents.emit('close');
+		};
+
+		this.recv = () => {
+			if (this.socket) return;
+
+			const data = (() => {
+				try {
+					return this.socket.read()
+				} catch (e) {
+					close();
+				}
+			})();
+
+			data && myEvents.emit('data', data);
+		};
+
+		this.send = () => {
+			if (!this.buffer || !this.socket) return;
+
+			try {
+				this.socket.send(this.buffer);
+			} catch (e) {
+				close();
+			}
+		};
+
+		Worker.runInBackground['__socket__' + (++Socket.__socketCounter)] = () => this.recv() || this.send() || true;
+
+	}
+
+	Object.defineProperty(Socket, 'buffer', {
+		get: function () {
+			return this.buffer;
+		},
+		set: function (obj) {
+			return this.buffer += obj && typeof obj === 'object' ? JSON.stringify(obj) : String(obj);
+		}
+	});
+
+	module.exports = Socket;
+}).call(null, module, require, Socket);
