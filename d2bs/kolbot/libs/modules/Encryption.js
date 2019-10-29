0ace2bb549b52913212d6c0c0756ce66b35c27de
jaenster <github@jaenster.net>
2019-10-29 15:56:56 +0100

    More usefull format

--- d2bs/kolbot/libs/modules/Encryption.js
+++ d2bs/kolbot/libs/modules/Encryption.js
@@ -7,28 +7,28 @@
 
 (function (module, require) {
 
-	module.exports = {
+	/** @param {string} data;
+	 * @param {string} cypher
+	 * @return {string} */
+	const encrypt =
+		(data, cypher) => Array.from(data).map((v, i) => String.fromCharCode(v.charCodeAt(0) ^ cypher.charCodeAt(i % cypher.length))).join('');
 
-		/** @param {string} data;
-		 * @param {string} cypher
-		 * @return {string} */
-		encrypt: (data, cypher) => Array.from(data).map((v, i) => String.fromCharCode(v.charCodeAt(0) ^ cypher.charCodeAt(i % cypher.length))).join(''),
+	/** @param {string} input
+	 * @returns {string}
+	 */
+	encrypt.pack = input => Array.from(input).map(char => ("000" + char.charCodeAt(0).toString(16)).slice(-4)).join('');
 
-		/** @param {string} input
-		 * @returns {string}
-		 */
-		pack: input => Array.from(input).map(char => ("000" + char.charCodeAt(0).toString(16)).slice(-4)).join(''),
+	/** @param {string} input -- hex'd string
+	 * @returns {string}
+	 */
+	encrypt.unpack = input => (input.match(/.{1,4}/g) || []).map(hex => String.fromCharCode(parseInt(hex, 16))).join(''),
 
-		/** @param {string} input -- hex'd string
-		 * @returns {string}
-		 */
-		unpack: input => (input.match(/.{1,4}/g) || []).map(hex => String.fromCharCode(parseInt(hex, 16))).join(''),
-	};
+		module.exports = encrypt;
 
 	/*
 		Tested in node
 
-		console.log(module.exports.unpack(module.exports.pack('test')));
-		console.log(module.exports.encrypt(module.exports.encrypt('test', 'supersecret'), 'supersecret'));
+		console.log(encrypt.unpack(encrypt.pack('test')));
+		console.log(encrypt(encrypt('test', 'supersecret'), 'supersecret'));
 	 */
 }).call(null, module, require);
