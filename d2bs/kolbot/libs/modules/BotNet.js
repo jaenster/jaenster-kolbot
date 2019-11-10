61047e4acd6f79341c4fa91741621e89da271e3b
jaenster <github@jaenster.net>
2019-11-10 16:17:24 +0100

    Small fixes on BotNet

--- d2bs/kolbot/libs/modules/BotNet.js
+++ d2bs/kolbot/libs/modules/BotNet.js
@@ -4,8 +4,28 @@
  *
  */
 (function (module, require) {
+	require('Debug');
 
+	const Events = require('Events');
+	const globalEvents = new Events;
+	const inGameEvents = new Events;
+	const exports = {
+		on: globalEvents.on,
+		off: globalEvents.off,
+		once: globalEvents.once,
+		register: channel => Message.send({BotNet: {register: channel}}),
+		send: (channel, data) => {
+			Message.send({BotNet: {send: {channel: {data: data, name: channel}}}})
+		},
+		inGame: {
+			send: data => exports.send(me.mapid, data),
+			on: inGameEvents.on,
+			off: inGameEvents.off,
+			once: inGameEvents.once,
+		},
+	};
 	const Message = require('Messaging');
+
 	if (getScript.startAsThread() === 'thread') {
 		const Socket = require('Socket');
 
@@ -14,7 +34,8 @@
 
 		// Override the send function, so we can just send data blobs
 		((orgSend, fakeObj) => socket.send = data => {
-			fakeObj = JSON.stringify(data);
+			if (data === undefined) return;
+			fakeObj = JSON.stringify(data)+String.fromCharCode(10,13);
 			orgSend.call(orgSend, fakeObj);
 		})(socket.send);
 
@@ -22,8 +43,6 @@
 		// Respond on message's from other threads
 		Message.on('BotNet', function (data) {
 			if (data.hasOwnProperty('register') && typeof data.register === 'string') socket.send({register: data.register});
-			print(' Data? ');
-			print(JSON.stringify(data));
 			if (data.hasOwnProperty('send') && typeof data.send === 'object' && data.send) {
 				socket.send(data.send)
 			}
@@ -46,25 +65,25 @@
 			if (me.hasOwnProperty('mapid') && me.mapid && mapid !== me.mapid) {
 				mapid && socket.send({unregister: mapid});
 				socket.send({register: (mapid = me.mapid)});
-
 			}
 		}
 
 	} else {
-		require('Debug');
-		const myEvents = new (require('Events'));
-		module.exports = {
-			on: myEvents.on,
-			off: myEvents.off,
-			register: channel => Message.send({BotNet: {register: channel}}),
-			send: (channel, data) => {
-				print(channel);
-				print(data);
-				Message.send({BotNet: {send: {channel: {data: data, name: channel}}}})
-			}
-		};
+		module.exports = exports;
+		Message.on('BotNet', function (data) {
 
-		Message.on('BotNet', data => data.hasOwnProperty('emit') && Array.isArray(data.emit) && myEvents.emit.apply(myEvents, data.emit));
+			// Trigger global events
+			globalEvents.emit(null, data.emit);
+
+			// Hook specifically on a channel
+			if (data.hasOwnProperty('emit') && data.emit.hasOwnProperty('channel') && data.emit.hasOwnProperty('data')) {
+				// Trigger as channel event
+				globalEvents.emit(data.channel, data.emit.data);
+
+				// Trigger upon a key, for ingame events, like Team and Messaging (so BotNet.inGame.on('Baal',data => {}));
+				me.mapid === data.emit.channel && Object.keys(data.emit.data).forEach(key => inGameEvents.emit(key, data.emit.data[key]));
+			}
+		});
 	}
 
 
