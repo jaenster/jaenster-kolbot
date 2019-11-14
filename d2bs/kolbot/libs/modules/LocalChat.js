b586b7afaf61b35f664029086ab63698c8932498
jaenster <github@jaenster.net>
2019-11-14 19:11:15 +0100

    localchat has a globalchat function now (untested)

--- d2bs/kolbot/libs/modules/LocalChat.js
+++ d2bs/kolbot/libs/modules/LocalChat.js
@@ -6,17 +6,18 @@
 module.exports = new function () {
 	const Config = require('Config');
 	const PacketBuilder = require('PacketBuilder');
-	
+	const Channel = require('Channel');
+
 	const LOCAL_CHAT_ID = 0xD2BAAAA;
 
-	let toggle, proxy = say;
+	let proxy = global['say'];
 
-	let relay = (msg) => D2Bot.shoutGlobal(JSON.stringify({
+	let relay = (msg) => Config.LocalChat.Mode < 4 ? D2Bot.shoutGlobal(JSON.stringify({
 		msg: msg,
 		realm: me.realm,
 		charname: me.charname,
 		gamename: me.gamename
-	}), LOCAL_CHAT_ID);
+	}), LOCAL_CHAT_ID) : (Channel.inGame.send({LocalChat: {charname: me.charname, msg: msg}}));
 
 	let onChatInput = (speaker, msg) => {
 		relay(msg);
@@ -31,44 +32,33 @@ module.exports = new function () {
 		msg = JSON.parse(msg);
 
 		if (me.gamename === msg.gamename && me.realm === msg.realm) {
-			new PacketBuilder().byte(38).byte(1, me.locale).word(2, 0, 0).byte(90).string(msg.charname, msg.msg).get();
+			spoof(msg);
 		}
 	};
+	const spoof = msg => new PacketBuilder().byte(38).byte(1, me.locale).word(2, 0, 0).byte(90).string(msg.charname, msg.msg).get();
 
-	let onKeyEvent = (key) => {
-		if (toggle === key) {
-			this.init(true);
-		}
-	};
+	Config.LocalChat.Mode = (Config.LocalChat.Mode) % 3;
+	print("ÿc2LocalChat enabled. Mode: " + Config.LocalChat.Mode);
 
-	this.init = (cycle = false) => {
-		if (!Config.LocalChat.Enabled) {
-			return;
-		}
-
-		Config.LocalChat.Mode = (Config.LocalChat.Mode + cycle) % 3;
-		print("ÿc2LocalChat enabled. Mode: " + Misc.config.LocalChat.Mode);
-
-		switch (Config.LocalChat.Mode) {
-			case 2:
-				removeEventListener("chatinputblocker", onChatInput);
-				addEventListener("chatinputblocker", onChatInput);
-			case 1:
-				removeEventListener("copydata", onChatRecv);
-				addEventListener("copydata", onChatRecv);
-				global['say'] = (msg, force = false) => force ? proxy(msg) : relay(msg);
-				break;
-			case 0:
-				removeEventListener("chatinputblocker", onChatInput);
-				removeEventListener("copydata", onChatRecv);
-				global['say'] = proxy;
-				break;
-		}
-
-		if (Config.LocalChat.Toggle) {
-			toggle = typeof Config.LocalChat.Toggle === 'string' ? Config.LocalChat.Toggle.charCodeAt(0) : Config.LocalChat.Toggle;
-			Config.LocalChat.Toggle = false;
-			addEventListener("keyup", onKeyEvent);
-		}
-	};
+	switch (Config.LocalChat.Mode) {
+		case 4:
+			Channel.inGame.on('LocalChat', function (data) {
+				data.hasOwnProperty('charname') && data.hasOwnProperty('msg') && spoof(data);
+			});
+			global['say'] = (msg, force = false) => force ? proxy(msg) : relay(msg);
+			break;
+		case 2:
+		case 1:
+			Config.LocalChat.Mode === 2 && removeEventListener("chatinputblocker", onChatInput);
+			Config.LocalChat.Mode === 2 && addEventListener("chatinputblocker", onChatInput);
+			removeEventListener("copydata", onChatRecv);
+			addEventListener("copydata", onChatRecv);
+			global['say'] = (msg, force = false) => force ? proxy(msg) : relay(msg);
+			break;
+		case 0:
+			removeEventListener("chatinputblocker", onChatInput);
+			removeEventListener("copydata", onChatRecv);
+			global['say'] = proxy;
+			break;
+	}
 };
