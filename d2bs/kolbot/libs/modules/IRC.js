93ebc31be9ec4c72da6bdc5fcc211acf01155163
jaenster <github@jaenster.net>
2019-11-16 14:49:49 +0100

    IRC module

---
+++ d2bs/kolbot/libs/modules/IRC.js
@@ -0,0 +1,75 @@
+/**
+ * @author Jaenster
+ * @description Simple IRC library that connects to IRC, and not much more as that
+ *
+ * @config
+
+ 	const client = new IRC({
+ 		server: 'irc.whatever.org',
+ 	})
+
+ 	client.on('msg',function(data) {
+
+ 	});
+
+ */
+(function (module, require) {
+	const defaultSettings = {
+		server: undefined,
+		nick: me.windowtitle,
+		user: 'D2BS',
+		port: 6667,
+		channel: '#d2bs',
+	};
+	module.exports = function (config) {
+		Object.keys(defaultSettings).filter(key => !config.hasOwnProperty(key)).forEach(key => config[key] = defaultSettings[key]);
+		Object.keys(config).forEach(key=>this[key]=config[key]);
+
+		if (!config.server) throw new Error('Need a server to connect to');
+
+		const Events = require('Events');
+		const myEvents = new Events();
+		Object.keys(myEvents).forEach(key=>this[key]=myEvents[key]);
+
+		/** @type Socket*/
+		const socket = new (require('Socket'))(config.server,config.port);
+
+		// Override the socket's send function, so it always ends with and crlf
+		(orgSend => socket.send = data => data !== undefined && orgSend.call(orgSend, data.toString() + String.fromCharCode(13, 10)))(socket.send);
+
+		// Override the connnect function
+		(orgSocket => socket.connect = () => {
+			try {
+				orgSocket.apply(socket);
+				socket.send('NICK '+this.nick);
+				socket.send('USER '+this.user+' :A Diablo 2 Bot');
+				print('Connected to ' + hostname);
+			} catch (e) {
+				// Dont care for a failed connection
+				print('Failed to connect to ' + hostname + ' (' + e.message + ')');
+			}
+		})(socket.connect);
+
+		socket.connect();
+
+		socket.on('data', function (data) {
+			data.split(String.fromCharCode(13, 10)).forEach(function(line) {
+				this.emit(null,line);
+				const word = line.split(' ').first();
+				switch(word) {
+					case 'PING':
+						socket.send(line.replace('PONG','PING')); // reply on a PING msg
+						break;
+					case '001':
+						socket.send('JOIN '+this.channel);
+						break;
+					case 'PRIVMSG':
+						this.emit('msg',line);
+						break;
+				}
+			});
+		});
+
+		this.send = socket.send;
+	}
+}).call(null, module, require);
