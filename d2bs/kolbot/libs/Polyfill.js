5881c6082772432bf918f5328f84804ec65f0a54
Ryan Giunchi <ryancrunchi@gmail.com>
2019-11-19 11:31:56 +0100

    Removed duplicated polyfill functions. Added Array intersection and difference functions.

--- d2bs/kolbot/libs/Polyfill.js
+++ d2bs/kolbot/libs/Polyfill.js
@@ -1,51 +1,4 @@
 
-// https://tc39.github.io/ecma262/#sec-array.prototype.findindex
-if (!Array.prototype.findIndex) {
-	Object.defineProperty(Array.prototype, 'findIndex', {
-		value: function (predicate) {
-			// 1. Let O be ? ToObject(this value).
-			if (this == null) {
-				throw new TypeError('"this" is null or not defined');
-			}
-
-			var o = Object(this);
-
-			// 2. Let len be ? ToLength(? Get(O, "length")).
-			var len = o.length >>> 0;
-
-			// 3. If IsCallable(predicate) is false, throw a TypeError exception.
-			if (typeof predicate !== 'function') {
-				throw new TypeError('predicate must be a function');
-			}
-
-			// 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-			var thisArg = arguments[1];
-
-			// 5. Let k be 0.
-			var k = 0;
-
-			// 6. Repeat, while k < len
-			while (k < len) {
-				// a. Let Pk be ! ToString(k).
-				// b. Let kValue be ? Get(O, Pk).
-				// c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-				// d. If testResult is true, return k.
-				var kValue = o[k];
-				if (predicate.call(thisArg, kValue, k, o)) {
-					return k;
-				}
-				// e. Increase k by 1.
-				k++;
-			}
-
-			// 7. Return -1.
-			return -1;
-		},
-		configurable: true,
-		writable: true
-	});
-}
-
 String.prototype.lcsGraph = function (compareToThis) {
 	if (!this.length || !compareToThis || !compareToThis.length) {
 		return null;
@@ -275,21 +228,31 @@ Array.prototype.compactMap = function (callback) {
 		}
 		return callback(x, i, array);
 	})
-		.filterNull();
-};
-
-Array.prototype.compactMap = function (callback) {
-	return this.map((x, i, array) => {
-		if (x == null) {
-			return null;
-		}
-		return callback(x, i, array);
-	})
-		.filterNull();
+	.filterNull();
 };
 
 Array.prototype.random = function () {
 	return this[Math.floor((Math.random() * this.length))];
+};
+
+Array.prototype.includes = function (e) {
+	return this.indexOf(e) > -1;
+};
+
+Array.prototype.contains = Array.prototype.includes;
+
+Array.prototype.intersection = function (other) {
+	return this.filter(e => other.includes(e))
+};
+
+Array.prototype.difference = function (other) {
+	return this.filter(e => !other.includes(e))
+};
+
+Array.prototype.symmetricDifference = function (other) {
+	return this
+		.filter(e => !other.includes(e))
+		.concat(other.filter(e => !this.includes(e)))
 };
 
 
