e5313eab0a86ca012ba0e3c68f138f514dbaed69
Ryan Giunchi <ryancrunchi@gmail.com>
2019-11-25 15:35:47 +0100

    Fixed Pickit using Config.LowGold

--- d2bs/kolbot/libs/modules/Pickit.js
+++ d2bs/kolbot/libs/modules/Pickit.js
@@ -35,7 +35,7 @@
 
 		// If total gold is less than 10k pick up anything worth 10 gold per
 		// square to sell in town.
-		if (rval.result === 0 && Town.ignoredItemTypes.indexOf(unit.itemType) === -1 && me.gold < Config.LowGold && unit.itemType !== 39) {
+		if (rval.result === 0 && Town.ignoredItemTypes.indexOf(unit.itemType) === -1 && me.lowGold && unit.itemType !== 39) {
 			// Gold doesn't take up room, just pick it up
 			if (unit.classid === 523) {
 				return {
