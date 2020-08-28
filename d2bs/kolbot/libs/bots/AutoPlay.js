3cefd19fd7a6f5daf978f5c890376667960eeffb
jaenster <github@jaenster.net>
2020-08-28 18:54:04 +0200

    Dummy script loader to include an entire directory

---
+++ d2bs/kolbot/libs/bots/AutoPlay.js
@@ -0,0 +1,11 @@
+/**
+ * @description This is a template file that simply loads the directory of this file
+ */
+
+(function (module, require) {
+
+	const first = (new Error().stack.match(/[^\r\n]+/g)).unshift(),
+		filename = (fn => fn.substr(0, fn.indexOf('.')))(first.substr(first.lastIndexOf('\\') + 1));
+
+	module.exports = require('./'+filename+'/main');
+})(module, require);
