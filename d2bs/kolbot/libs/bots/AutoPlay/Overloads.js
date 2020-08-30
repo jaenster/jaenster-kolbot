f2be87f810749a436a0184af5144e7d9c8090336
jaenster <github@jaenster.net>
2020-08-30 14:24:04 +0200

    write as classform

--- d2bs/kolbot/libs/bots/AutoPlay/Overloads.js
+++ d2bs/kolbot/libs/bots/AutoPlay/Overloads.js
@@ -5,40 +5,55 @@
 
 
 	function Overload(Obj, name, fn) {
-		console.debug('Overloading ' + Obj.name + '.' + name);
 		this.obj = Obj;
 		this.name = name;
 		this.original = Obj[fn];
 		this.fn = fn;
+		this.installed = false;
 
-		Obj[fn] = fn.bind(Obj, this.original);
+
+		Overload.instances.push(this);
 	}
+	// static variable
+	Overload.instances = [];
 
-	Overload.prototype.rollback = function () {
-		this.obj[this.name] = this.original;
+	Overload.prototype.remove = function () {
+		if (this.installed) {
+			console.debug('removing ' + this.name + '.' + name);
+			this.obj[this.name] = this.original;
+
+			this.installed = false;
+		}
+	};
+
+	Overload.prototype.install = function () {
+		if (!this.installed) {
+			console.debug('Overloading ' + this.name + '.' + name);
+			this.obj[this.name] = this.fn.bind(this.obj, this.original);
+
+			this.installed = true;
+		}
 	};
 
 	module.exports = function (Config, Attack, Pickit, Pather, Town, Misc) {
+		const from = Overload.instances.length;
+		new Overload(Pather, 'getWP', /**@this Pather*/ function (original, ...args) {
 
-		const overloads = [
-			new Overload(Pather, 'getWP', /**@this Pather*/ function (original, ...args) {
-
-				// If we can teleport we just use the original Pather.getWP
-				if (this.useTeleport()) {
-					// call original function for now, overload in the future
-					original.call(Pather, args);
-				}
-			}),
-		];
+			// If we can teleport we just use the original Pather.getWP
+			if (this.useTeleport()) {
+				// call original function for now, overload in the future
+				original.call(Pather, args);
+			}
+		});
 
 
+		Overload.instances.slice(from).forEach(ol => ol.install());
 		return {
 			rollback: () => {
-				overloads.forEach(el => el.rollback());
+				Overload.instances.forEach(el => el.remove());
 			}
 		}
-
-	}
-
+	};
+	module.exports.Overload = Overload;
 
 })(module, require);
